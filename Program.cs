5152bc0b5a1720aec68929febb1bfb87103fe431
svetoslavr <61112568+svetoslavr@users.noreply.github.com>
2020-02-28 12:24:38 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp75
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int jump = int.Parse(Console.ReadLine());
+            jump++;
+            const char Search = 'p';
+            bool hasMatch = false;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == Search)
+                {
+                    hasMatch = true;
+
+                    int endIndex = jump;
+
+
+                    if (endIndex > text.Length)
+                    {
+                        endIndex = text.Length;
+                    }
+
+                    string matchedString = text.Substring(i, endIndex);
+                    Console.WriteLine(matchedString);
+                    i += jump;
+                }
+            }
+
+            if (!hasMatch)
+            {
+                Console.WriteLine("no");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
