f463a356de588c83fb126a3988729b49449ba332
svetoslavr <61112568+svetoslavr@users.noreply.github.com>
2020-04-01 16:56:08 +0300

    Add files via upload
    Homework 02.04.20

---
+++ homework 02.04.20/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cols = int.Parse(Console.ReadLine());
+            int rows = int.Parse(Console.ReadLine());
+
+            int[,] array = new int[cols, rows];
+
+            for (int a = 0; a < cols; a++)
+            {
+            r:
+                int[] vs = null;
+                try
+                {
+                    vs = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                }
+                catch (Exception e) { Console.WriteLine("Format error"); goto r; }
+                for (int i = 0; i < rows; i++)
+                {
+                    array[a, i] = vs[i];
+                }
+            }
+
+            Console.WriteLine("===========");
+
+            for (int i = 0; i < cols; i++)
+            {
+                string rdy = "";
+                for (int f = 0; f < rows; f++)
+                {
+                    rdy += array[i, f] + " ";
+                }
+                Console.WriteLine(rdy);
+            }
+            Console.WriteLine(smallest(array, cols, rows));
+            Console.ReadLine();
+        }
+
+        public static string smallest(int[,] array, int rows, int cols)
+        {
+            string p = "";
+
+            for (int i = 0; i < cols; i++)
+            {
+                int smallest = array[0, i];
+                for (int f = 0; f < rows; f++)
+                {
+                    if (array[f, i] < smallest)
+                    {
+                        smallest = array[f, i];
+                    }
+                }
+                p += smallest + " ";
+            }
+            return p;
+        }
+    }
+}
