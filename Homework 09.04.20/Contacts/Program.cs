bfbc339949b9bfef1f6ccaffcee2332dbce35a3f
svetoslavr <61112568+svetoslavr@users.noreply.github.com>
2020-04-08 17:59:32 +0300

    Add files via upload
    Homework 09.04.20

---
+++ Homework 09.04.20/Contacts/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> contacts = new Dictionary<string, string>();
+            while (true)
+            {
+                var a = Console.ReadLine().Split(' ');
+                string contact = a[0].ToLower();
+                switch (contact)
+                {
+                    case "A":
+                        {
+                            contacts.Add(a[1], a[2]);
+                            break;
+                        }
+                    case "S":
+                        {
+                            if (contacts.ContainsKey(a[1]))
+                            {
+                                Console.WriteLine($"{a[1]} --> {contacts[a[1]]}");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"This contact does not exist.");
+                            }
+                            break;
+                        }
+                    case "END":
+                        {
+                            return;
+                        }
+                }
+               
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework 09.04.20/Metals/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> resources = new Dictionary<string, int>();
+            resources.Add("Gold", 0);
+            resources.Add("Silver", 0);
+            resources.Add("Copper", 0);
+            while (true)
+            {
+                Console.Write(" Type of metal: ");
+                string met = Console.ReadLine().ToLower();
+                if ( met == "stop" || met   == "copper" || met == "silver" || met == "gold")
+                {
+                    if (met != "stop") 
+                    {
+                        int b = 0;
+                        Console.Write($"Enter b {met}: ");
+                        try
+                        {
+                            b = int.Parse(Console.ReadLine());
+                        }
+                        catch (FormatException e) { Console.WriteLine(" Type a number:"); }
+                        switch (met)
+                        {                            
+                            case "copper":
+                                {
+                                    resources["Copper"] += b;
+                                    Console.WriteLine($"{b} copper added.");
+                                    break;
+                                }
+                            case "silver":
+                                {
+                                    resources["Silver"] += b;
+                                    Console.WriteLine($"{b} silver added.");
+                                    break;
+                                }
+                            case "gold":
+                                {
+                                    resources["Gold"] += b;
+                                    Console.WriteLine($"{b} gold added.");
+                                    break;
+                                }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Gold --> {resources["Gold"]}");     
+                        Console.WriteLine($"Silver --> {resources["Silver"]}"); 
+                        Console.WriteLine($"Copper --> {resources["Copper"]}"); 
+                        break;
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
