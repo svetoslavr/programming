5c8d726ac90fc26ce7b7a431f6b11f2d55919c8a
svetoslavr <61112568+svetoslavr@users.noreply.github.com>
2020-04-22 18:53:39 +0300

    Add files via upload
    Homework 22.04.2020

---
+++ Homework 22.04.2020/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice
+{
+    class Dice
+    {
+        Random number = new Random();
+        public int RollDice(int size)
+        {
+            int nums = number.Next(1, size + 1);
+            return nums;
+        }
+    }
+}
---
+++ Homework 22.04.2020/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dice number = new Dice();
+            int random = 0;
+
+            Console.Write("");
+            int dicenumber = int.Parse(Console.ReadLine());
+
+            random = number.RollDice(dicenumber);
+            Console.WriteLine( number);
+
+            Console.ReadLine();
+        }
+    }
+}
