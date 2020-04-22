60a094153d7028d9f56a37313083665a54848fb7
svetoslavr <61112568+svetoslavr@users.noreply.github.com>
2020-04-22 18:36:59 +0300

    Add files via upload
    Homework dice

---
+++ Homework dice/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice
+{
+    class Dice
+    {
+        Random number = new Random();
+        public int RollDice(int size)
+        {
+            int dice = number.Next(1, size + 1);
+            return dice;
+        }
+    }
+}
---
+++ Homework dice/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dice dice = new Dice();
+            int number = 0;
+
+            Console.Write("");
+            int size = int.Parse(Console.ReadLine());
+
+            number = dice.RollDice(size);
+            Console.WriteLine( number);
+
+            Console.ReadLine();
+        }
+    }
+}
